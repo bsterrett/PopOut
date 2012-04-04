30e3c9bc36ec83a31cf2c8ac338ea422c764d126
Ben <bs11868@gmail.com>
2012-04-04 00:09:58 -0400

    user input working, minimax search is breaking the game board.

--- PopOut/src/popout/control/Control.java
+++ PopOut/src/popout/control/Control.java
@@ -1,5 +1,7 @@
 package popout.control;
 
+import java.util.Scanner;
+
 import popout.board.*;
 import popout.search.*;
 import popout.ui.*;
@@ -7,23 +9,27 @@ import popout.ui.*;
 public class Control {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		BoardState B = new BoardState(6, 7);
 		CLDisplay C = new CLDisplay(6, 7, B);
-		//Search M = new Minimax(B);
-		B.drop(0, (short) 1);
-		B.drop(0, (short) 1);
-		B.drop(0, (short) 1);
-		B.drop(0, (short) 2);
-		B.drop(1, (short) 1);
-		B.drop(1, (short) 1);
-		B.drop(1, (short) 2);
-		System.out.println(C.toString());
+		Search M = new Minimax(B);
+		Scanner scan = new Scanner(System.in); 
+
 		while(B.compute_win() == 0){
-			B.drop(3, (short) 2);
-			System.out.println(C.toString());
-			System.out.println("Current winner: " + B.compute_win());
+			System.out.println("Next move? (Format: 'X Y' where X is D or P and Y is 0-6)");
+			String user_input = scan.nextLine();
+			if(user_input.length() > 0 &&'D' == user_input.charAt(0)){
+				B.drop(Integer.parseInt(user_input.substring(2)), (short) 1);
+			}
+			else if(user_input.length() > 0 && 'P' == user_input.charAt(0)){
+				B.pop(Integer.parseInt(user_input.substring(2)));
+			}
+			else{
+				System.err.println("Invalid user input!");
+			}
+			//M.make_next_move();
+			System.out.println(C.toString());			
 		}
+		System.out.println("The winner: " + B.compute_win());
 	}
 
 }
