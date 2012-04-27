5004a50c8a68c55530c0f81ec0462b9c399f6132
Ben <bs11868@gmail.com>
2012-04-27 00:01:00 -0400

    changing tie condition to be a const

--- src/popout/PlayerNum.java
+++ src/popout/PlayerNum.java
@@ -4,6 +4,7 @@ public class PlayerNum {
 	public static final short EMPTY_SPACE = 0;
 	public static final short HUMAN = 1;
 	public static final short COMPUTER = 2;
+	public static final short TIE = 21;
 	
 	public static short opposite(final short player){
 		return (short) (player == HUMAN ? COMPUTER : HUMAN);
